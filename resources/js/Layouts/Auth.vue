d09ea288e9025e9fe8b90d2d11507234fbfdca22
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-24 18:46:04 -0400

    Create and start auth layout and login view

---
+++ resources/js/Layouts/Auth.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+        <header>
+            <Link href="/">Entrar</Link>
+            <Link href="/">Cadastrar</Link>
+        </header>
+        <article>
+            <slot />
+        </article>
+    </main>
+</template>
+  
+<script>
+
+import { Link } from '@inertiajs/inertia-vue3'
+
+export default {
+    components: {
+        Link,
+    }
+}
+</script>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>LOGIN</h1>
+
+    <Head title='Acessar conta' />
+    <div>
+        <H1>LOGIN</H1>
+        <p>Hello, welcome to your first Inertia app!</p>
+    </div>
 </template>
+  
+<script>
+
+import Layout from './../../Layouts/Auth.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+
+export default {
+    components: { Head },
+    layout: (h, page) => h(Layout, () => child), // Using a render function
+    layout: Layout, // Using the shorthand
+
+    props: {
+        user: Object,
+    },
+}
+</script>
