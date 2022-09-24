49d9ac9c991abdf5903392ed694ac6de54e34937
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-24 18:26:42 -0400

    Laravel with Inertia(Vue3) started

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return Inertia::render("Auth/Login");
+    }
+}
