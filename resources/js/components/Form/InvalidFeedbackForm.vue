ba05b503ce5be0ad3b810dd782b243bb974c3fbc
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-28 23:11:42 -0400

    Form components update

---
+++ resources/js/components/Form/InvalidFeedbackForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <small class="invalid-feedback">
+        {{ errorMessage }}
+    </small>
+</template>
+
+<script>
+
+export default {
+    props: {
+        errorMessage: { type: String, default: null }
+    }
+};
+
+</script>
---
+++ resources/js/components/Form/LabelForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <label v-if="label" :for="name" class="mb-1">
+        {{ label }}
+    </label>
+</template>
+
+<script>
+
+export default {
+    props: {
+        label: { type: String, default: null },
+        name: { type: String, default: null },
+    }
+};
+
+</script>
