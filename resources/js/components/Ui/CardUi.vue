51a3eadbc241e362136932b38f0f8dc023a3d33f
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-10-01 15:19:15 -0400

    Card component.

---
+++ resources/js/components/Ui/CardUi.vue
@@ -0,0 +1,59 @@
+<template>
+
+    <div @mouseover="hover=true" @mouseout="hover=false" :class="cardStyle">
+        <div v-if="icon" :class="iconCardStyle">
+            <IconUi :icon="icon" />
+        </div>
+        <div class="ms-2">
+            <h2 class="fs-5 fw-semibold mb-1" v-html="title"></h2>
+            <div v-if="$slots.content">
+                <slot name="content" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import IconUi from './IconUi.vue';
+
+export default {
+    components: { IconUi },
+    props: {
+        icon: { type: String, default: null },
+        title: { type: String, default: null },
+        border: { type: Boolean, default: false },
+        noShadow: { type: Boolean, default: false },
+    },
+
+    data() {
+        return {
+            hover: false
+        };
+    },
+
+    computed: {
+        iconCardStyle() {
+            return `d-flex justify-content-center align-items-center fs-1 bg-dark text-light rounded icon-card`;
+        },
+        cardStyle() {
+            return `card card-body ${!this.border ? 'border-0' : ''} ${this.noShadow ? '' : 'shadow-sm'} ${this.hover ? 'shadow-lg' : ''} flex-row align-items-center`;
+        }
+    }
+};
+
+</script>
+
+<style>
+.icon-card {
+    min-width: 50px;
+    min-height: 50px;
+}
+
+@media(min-width:768px) {
+    .icon-card {
+        min-width: 75px;
+        min-height: 75px;
+    }
+}
+</style>
