d6d61a30d84949111895a56b47a2cc9ad93580c4
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-10-07 18:53:39 -0400

    Component for image preview.

---
+++ resources/js/components/Ui/ImagePreviewUi.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-100 d-flex justify-content-center">
+        <div :class="[{'border': !borderless}, 'd-flex justify-content-center align-items-center mb-1']"
+            style="width: 100%; max-width: 200px; height: 100px; overflow: hidden;">
+            <img class="img-fluid" v-if="previewUrl" :src="previewUrl" :alt="previewAlt">
+            <span v-else class="text-muted" v-html="previewAlt"></span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        borderless: { type: Boolean, default: false },
+        previewUrl: { type: String, default: null },
+        previewAlt: { type: String, default: "Preview" },
+    }
+};
+
+</script>
