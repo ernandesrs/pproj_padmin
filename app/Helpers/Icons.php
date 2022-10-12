ff9e4bdb9698c16113e99705e489e6055158a46c
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-10-11 20:54:51 -0400

    A config file and helper to icons

---
+++ app/Helpers/Icons.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Helpers;
+
+class Icons
+{
+    /**
+     * Get a element icon
+     *
+     * @param string $name
+     * @param string $tag
+     * @return string
+     */
+    public static function elem(string $name, string $tag = "i")
+    {
+        return "<{$tag} class='icon " . (self::get($name) ?? self::get("app")) . "'></{$tag}>";
+    }
+
+    /**
+     * Get icon on a icons list
+     *
+     * @param string $name
+     * @return string|null
+     */
+    public static function get(string $name)
+    {
+        return config("app-icons." . $name);
+    }
+
+    /**
+     * Get icons as array
+     *
+     * @return array
+     */
+    public static function toArray()
+    {
+        return config("app-icons");
+    }
+
+    /**
+     * Get icons as array
+     *
+     * @return object
+     */
+    public static function toObject()
+    {
+        return (object) config("app-icons");
+    }
+
+    /**
+     * Get icons as JSON
+     *
+     * @return string
+     */
+    public static function toJson()
+    {
+        return json_encode(config("app-icons"));
+    }
+}
