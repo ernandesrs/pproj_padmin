6f1c73d2512659184cb5b7df80cbb9763cbec0c4
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-11-09 11:27:25 -0300

    Controller, model, migrations for menu

---
+++ database/migrations/2022_11_09_135501_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 25)->unique()->nullable(false);
+            $table->json('items')->nullable(false)->default(json_encode([]));
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menus');
+    }
+}
