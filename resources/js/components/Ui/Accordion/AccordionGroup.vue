49d84c0d0238bda1ff27c3bdcdd41798d0b9bc4d
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-11-12 08:08:03 -0300

    Bootstrap accordion component

---
+++ resources/js/components/Ui/Accordion/AccordionGroup.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="accordion" :id="id">
+        <slot />
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        id: { type: [String, Number], default: "accordion" + (new Date()).getTime() }
+    },
+};
+
+</script>
---
+++ resources/js/components/Ui/Accordion/AccordionItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="accordion-item">
+        <h2 class="accordion-header" :id="['heading' + id]">
+            <button :class="['accordion-button', { 'collapsed': !show }]" type="button"
+                data-bs-toggle="collapse" :data-bs-target="['#collapse' + id]">
+                {{ headerText }}
+            </button>
+        </h2>
+        <div :id="['collapse' + id]"
+            :class="['accordion-collapse collapse', { 'show': show }]"
+            :data-bs-parent="['#' + this.$parent.$props.id]">
+            <div class="accordion-body">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        headerText: { type: String, default: null },
+        id: { type: [String, Number], default: null },
+        show: { type: Boolean, default: false },
+    }
+};
+
+</script>
