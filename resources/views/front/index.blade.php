f3fcdfbb49855e5c7e7747f980c42885b9ecf2c6
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-11-15 20:23:10 -0300

    Add section 2

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -13,8 +13,8 @@
                     </p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                         @foreach ($section_1->buttons as $button)
-                            <a href="{{ $button->url }}"
-                                class="btn btn-{{ $button->style }} btn-lg px-4 me-md-2 fw-semibold">
+                            <a href="{{ $button->url }}" class="btn btn-{{ $button->style }} btn-lg px-4 me-md-2 fw-semibold"
+                                target="{{ $button->target }}" title="{{ $button->title }}">
                                 @if (($button->align ?? 'start') == 'start')
                                     {!! $button->icon !!} <span>{{ $button->text }}</span>
                                 @else
@@ -27,6 +27,41 @@
                 <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
                     <img class="rounded-lg-3" src="{{ Storage::url($section_1->content->image) }}" width="525"
                         alt="">
+                </div>
+            </div>
+        </div>
+    @endif
+
+    @if ($section_2)
+        <div class="container py-5">
+            <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3">
+                <div class="col-lg-4 p-0">
+                    <img class="img-fluid rounded-lg-3" src="{{ Storage::url($section_2->content->image) }}" width="525"
+                        alt="">
+                </div>
+                <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+                    <h1 class="display-4 fw-semibold lh-1 text-dark-dark">
+                        {{ $section_2->title }}
+                    </h1>
+                    <h2>
+                        {{ $section_2->subtitle }}
+                    </h2>
+                    <p class="lead text-dark-light fw-normal pb-3">
+                        {!! $section_2->content->content !!}
+                    </p>
+                    <div class="pt-2">
+                        @foreach ($section_2->buttons as $button)
+                            <a href="{{ $button->url }}"
+                                class="btn btn-{{ $button->style }} btn-lg px-4 me-md-2 fw-semibold"
+                                target="{{ $button->target }}" title="{{ $button->title }}">
+                                @if (($button->align ?? 'start') == 'start')
+                                    {!! $button->icon !!} <span>{{ $button->text }}</span>
+                                @else
+                                    <span>{{ $button->text }}</span> {!! $button->icon !!}
+                                @endif
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
