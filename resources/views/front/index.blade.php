f1060abbe6474ee6fcbf539d6fc909c1a2fc2f17
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-11-15 20:27:37 -0300

    Update section 1

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,12 +5,12 @@
         <div class="container py-5">
             <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3">
                 <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-                    <h1 class="display-4 fw-semibold lh-1 text-dark-dark">
+                    <h1 class="display-4 fw-semibold lh-1 mb-4 text-dark-dark">
                         {{ $section_1->title }}
                     </h1>
-                    <p class="lead text-dark-light fw-normal pb-3">
+                    <div class="lead text-dark-light fw-normal pb-3">
                         {!! $section_1->content->description !!}
-                    </p>
+                    </div>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
                         @foreach ($section_1->buttons as $button)
                             <a href="{{ $button->url }}" class="btn btn-{{ $button->style }} btn-lg px-4 me-md-2 fw-semibold"
