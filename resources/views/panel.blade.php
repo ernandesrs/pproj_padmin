6b2db79be35c0e5be1685eaad94053de725bb3ee
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-12-03 09:13:21 -0300

    Favicon

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
     <link href="{{ asset('css/panel/app.css') }}" rel="stylesheet" />
+
+    @php
+        $settings = \App\Models\Content::where('name', 'front_settings')->first();
+    @endphp
+    @if ($settings && $settings->content->header->favicon ?? null)
+        <link rel="shortcut icon" href="{{ Storage::url($settings->content->header->favicon) }}" type="image/x-icon">
+    @endif
+
     @routes
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
