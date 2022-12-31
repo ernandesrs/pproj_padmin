5ddf13730777d502f1cd1b532c0a998a0aa15cd6
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-12-31 06:40:13 -0300

    Role model and migration

---
+++ app/Models/Admin/Role.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    public const RULES = ['view', 'viewAny', 'create', 'update', 'delete', 'forceDelete', 'restory'];
+    public const RULABLES = [
+        'role',
+        'service',
+        'image',
+        'video',
+        'section',
+        'content',
+        'menu',
+        'page',
+        'user'
+    ];
+
+    /**
+     * Create role
+     *
+     * @param array $validated
+     * @return Role
+     */
+    public function create(array $validated)
+    {
+        $this->name = $validated['name'];
+        $this->rulables = json_encode($this->setRules($validated['rulables']));
+        $this->save();
+
+        return $this;
+    }
+
+    /**
+     * Update role
+     *
+     * @param array $attributes
+     * @param array $options
+     * @return bool
+     */
+    public function update(array $attributes = [], array $options = [])
+    {
+        $attributes['rulables'] = json_encode($this->setRules($attributes['rulables']));
+        return parent::update($attributes, $options);
+    }
+
+    /**
+     * Define rule to rullables
+     *
+     * @param array $rulables
+     * @return array
+     */
+    public function setRules(array $rulables)
+    {
+        $arr = array_fill_keys(self::RULABLES, array_fill_keys(self::RULES, false));
+
+        foreach ($rulables as $rulable => $rules) {
+            foreach ($rules as $ruleKey => $ruleValue) {
+                $arr[$rulable][$ruleKey] = $ruleValue;
+            }
+        }
+
+        return $arr;
+    }
+
+    /**
+     * Booted
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::retrieved(function ($role) {
+            $role->rulables = json_decode($role->rulables);
+        });
+    }
+}
